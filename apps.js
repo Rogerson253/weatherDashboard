03d3eac9bf03f3fa6d35630b43ae9aed3923bbfe
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-03-15 12:54:56 -0500

    appended response text to page

--- apps.js
+++ apps.js
@@ -1,51 +1,55 @@
+// Variable for current date
 var today = moment().format("dddd, MMMM Do YYYY");
 
+// API Key
 var authKey = "68d666b75db062eebadc3796248de346"
 
 // Empty variable for user input
 var queryTerm = "";
 
-// Basic URL
+// Basic URL for weather
 var  queryURLBase = "https://api.openweathermap.org/data/2.5/weather?appid=68d666b75db062eebadc3796248de346&q=Nashville";
 
+// Basic URL for UV Index
 var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=68d666b75db062eebadc3796248de346&lat=36.17&lon=-86.78"
 
 // Calls object from OpenWeather for weather data
 function callWeather() {
-$.ajax({
-    url: queryURLBase,
-    method: "GET",
-})
-    .then(function(response){
-        console.log(queryURLBase);
-        console.log(response);
-        console.log(response.name);
-        console.log(today);
-        console.log(response.main.temp); 
-        console.log(response.wind.speed);
-        console.log(response.main.humidity);
-       
+    $.ajax({
+        url: queryURLBase,
+        method: "GET",
     })
-}
-
-
+        .then(function(response){
+            console.log(response.name);
+            console.log(today);
+            console.log(response.main.temp); 
+            console.log(response.wind.speed);
+            console.log(response.main.humidity);
+            $("#weatherBlock").append("<h3>" + response.name + "</h3>");
+            $("#weatherBlock").append("<h3>" + today + "</h3>");
+            $("#weatherBlock").append("<h3>" + response.main.temp + "</h3>");
+            $("#weatherBlock").append("<h3>" + response.wind.speed + "</h3>");
+            $("#weatherBlock").append("<h3>" + response.main.humidity + "</h3>");
+            callUv();
+           
+        })
+    }
+    
 // Calls object from OpenWeather for UV Index data
 function callUv() {
-$.ajax({
-    url: queryURL,
-    method: "GET",
-})
-    .then(function(response){
-        console.log(queryURL);
-        console.log(response);
-        console.log(response.value);
+    $.ajax({
+        url: queryURL,
+        method: "GET",
     })
-}
+        .then(function(response){
+            console.log(response.value);
+            $("#weatherBlock").append("<h3>" + response.value + "</h3>");
+        })
+    }
 
 
 
-
+// Onclick event that logs functions
 $("#getWeather").on("click", function(){
     callWeather();
-    callUv();
 })
