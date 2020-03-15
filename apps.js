b4d4b0d9e8322d1233097ff884d8959a37b01c25
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-03-15 12:05:27 -0500

    created ajax call for UV Index

--- apps.js
+++ apps.js
@@ -6,7 +6,9 @@ var queryTerm = "";
 // Basic URL
 var  queryURLBase = "https://api.openweathermap.org/data/2.5/weather?appid=68d666b75db062eebadc3796248de346&q=Nashville";
 
-// Calls object from OpenWeather
+var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=68d666b75db062eebadc3796248de346&lat=36.17&lon=-86.78"
+
+// Calls object from OpenWeather for weather data
 function callWeather() {
 $.ajax({
     url: queryURLBase,
@@ -15,6 +17,24 @@ $.ajax({
     .then(function(response){
         console.log(queryURLBase);
         console.log(response);
+        console.log(response.main.temp);
+        console.log(response.name);
+        console.log(response.wind.speed);
+        console.log(response.main.humidity);
+       
     })
 }
 callWeather();
+
+// Calls object from OpenWeather for UV Index data
+function callUv() {
+$.ajax({
+    url: queryURL,
+    method: "GET",
+})
+    .then(function(response){
+        console.log(response);
+        console.log(response.value);
+    })
+}
+callUv();
